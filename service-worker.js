2145d9bf77f5f4037ea3d3b1c9c9b3be396dca80
pikiwidigital <pikiwidigital@gmail.com>
2026-08-16 19:35:39 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "missao-musica-v26";
+const CACHE_NAME = "missao-musica-v27";
 
 const FILES_TO_CACHE = [
   "./",
@@ -17,7 +17,6 @@ const FILES_TO_CACHE = [
   "./missao.js",
   "./sounds.js",
 
-  "./missions.json",
   "./manifest.webmanifest",
 
   "./logo.png",
@@ -26,10 +25,12 @@ const FILES_TO_CACHE = [
 
   "./audio/button-click.mp3"
 ];
+
+
 /*
-  Guarda os ficheiros essenciais quando a aplicação
-  é aberta pela primeira vez.
+  Guarda os ficheiros essenciais para utilização offline.
 */
+
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -40,9 +41,11 @@ self.addEventListener("install", function (event) {
   self.skipWaiting();
 });
 
+
 /*
-  Remove versões antigas da cache quando atualizamos a aplicação.
+  Remove versões antigas da cache.
 */
+
 self.addEventListener("activate", function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
@@ -61,14 +64,52 @@ self.addEventListener("activate", function (event) {
   self.clients.claim();
 });
 
+
 /*
-  Procura primeiro o ficheiro guardado.
-  Se não estiver disponível, tenta obtê-lo através da internet.
+  MISSIONS.JSON
+
+  Tenta sempre obter primeiro a versão mais recente.
+  Se estiver offline, utiliza a versão guardada.
 */
+
 self.addEventListener("fetch", function (event) {
   if (event.request.method !== "GET") {
     return;
   }
+
+  const requestURL = new URL(event.request.url);
+
+  if (requestURL.pathname.endsWith("/missions.json")) {
+    event.respondWith(
+      fetch(event.request)
+        .then(function (networkResponse) {
+
+          const responseCopy =
+            networkResponse.clone();
+
+          caches.open(CACHE_NAME).then(function (cache) {
+            cache.put(
+              event.request,
+              responseCopy
+            );
+          });
+
+          return networkResponse;
+        })
+        .catch(function () {
+          return caches.match(event.request);
+        })
+    );
+
+    return;
+  }
+
+
+  /*
+    RESTANTES FICHEIROS
+
+    Usa primeiro a cache.
+  */
 
   event.respondWith(
     caches.match(event.request).then(function (cachedResponse) {
